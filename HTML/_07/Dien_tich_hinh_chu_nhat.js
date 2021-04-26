458f606682eebb7d020312634ab5418249263c17
LongPT <81423778+longpt8688@users.noreply.github.com>
2021-04-26 17:56:14 +0700

    Upload

---
+++ HTML/_07/Dien_tich_hinh_chu_nhat.js
@@ -0,0 +1,8 @@
+let inputwidth
+let inputheight
+inputwidth = prompt('Nhập Vào Chiều Dài Hình Chữ Nhật: ')
+inputheight = prompt('Nhập vào chiểu rộng hình chữ nhật: ')
+let width = parseInt(inputwidth)
+let height = parseInt(inputheight)
+let S = width*height;
+document.write('Diện tích hình chữ nhật = ' + S)
